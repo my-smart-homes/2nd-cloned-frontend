d70e022190551fb1ea8ac3a178f335fab93a37dd
Paul Bottein <paul.bottein@gmail.com>
2024-09-12 15:32:52 +0200

    Add full name

--- src/data/entity_registry.ts
+++ src/data/entity_registry.ts
@@ -42,6 +42,7 @@ export interface EntityRegistryDisplayEntryResponse {
     tk?: string;
     hb?: boolean;
     dp?: number;
+    hn?: boolean;
   }[];
   entity_categories: Record<number, EntityCategory>;
 }
